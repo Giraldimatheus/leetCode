697353c9d89f765c58f8eaa31afcbec744d1bb38
Matheus Giraldi <115491408+Giraldimatheus@users.noreply.github.com>
2024-10-01 13:23:39 -0300

    Create file

---
+++ 1-TwoSum/1-TwoSum.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+        Dictionary<int, int> numIndexMap = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (numIndexMap.ContainsKey(complement))
+            {
+                return new int[] { numIndexMap[complement], i };
+            }
+            numIndexMap[nums[i]] = i;
+        }
+
+        return new int[] { -1, -1 };
+    }
+}
