7c5ad73a80768a99f4fadb59117dfcaab42d3851
Matheus Giraldi <115491408+Giraldimatheus@users.noreply.github.com>
2024-10-02 15:00:06 -0300

    Update 2-AddTwoNumbers.cs

--- 2-AddTwoNumbers/2-AddTwoNumbers.cs
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cs
@@ -12,10 +12,10 @@
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) 
     {
-        ListNode list = new ListNode(0); // Nó fictício
-        // Ambos compartilham o mesmo nó fictício (endereço na memória), por isso que podemos retornar o list.next no   final.
+        ListNode list = new ListNode(0); // NÃ³ fictÃ­cio
+        // Ambos compartilham o mesmo nÃ³ fictÃ­cio (endereÃ§o na memÃ³ria), por isso que podemos retornar o list.next no final.
         ListNode curr = list;
-        // carry é o valor a esquerda da soma dos valores dos nós divido por 10. Esse valor é carregado para a próxima interação.
+        // carry Ã© o valor a esquerda da soma dos valores dos nÃ³s divido por 10. Esse valor Ã© carregado para a prÃ³xima interaÃ§Ã£o.
         int carry = 0;
         while (l1 != null || l2 != null)
         {
@@ -23,7 +23,7 @@ public class Solution {
             int y = (l2 != null) ? l2.val : 0;
             int sum = carry + x + y;
             carry = sum / 10;
-            // O next é o valor a direita da soma dos valores dos nós divido por 10.
+            // O next Ã© o valor a direita da soma dos valores dos nÃ³s divido por 10.
             curr.next = new ListNode(sum % 10);
             curr = curr.next;
             if (l1 != null) l1 = l1.next;
